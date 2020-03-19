e4709d7009598aacfeba615c196bfeb8805e74b6
Alejandro <a.gcastellanos@alumnos.upm.es>
2020-03-19 11:20:10 +0100

    FuncionLog

--- Código/minCuadPond.m
+++ Código/minCuadPond.m
@@ -1,17 +1,5 @@
 function w = minCuadPond(M)
 
-% Generar H a partir de M
-H;
 
-% Generar b: (0, 0, ..., 1)
-[fil, ~] = size(H);
-b = zeros(fil);
-b(length(b)) = 1;
-
-% Resolver con minimos cuadrados
-w = H\b;
-
-% Normalizar
-w = w/sum(w);
 
 end
