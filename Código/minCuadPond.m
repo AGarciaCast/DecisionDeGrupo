30e6bb942ac60f29b1ac5931b564175694b18516
Diego José Abengózar Vilar <diegojose.abengozar.vilar@alumnos.upm.es>
2020-03-20 12:29:22 +0100

    acabar metodo Pond

--- Código/minCuadPond.m
+++ Código/minCuadPond.m
@@ -1,5 +1,32 @@
 function w = minCuadPond(M)
 
+s=size(M);
+long= (s(1)*s(2))-s(1);
 
+H2 = zeros([long,s(2)]);
 
+% Generar b
+b=zeros([long+1,1]);
+b(long+1) = 1;
+
+k=1;
+for i= (1:s(1))
+    for j=1:s(2)
+        if (i ~= j)
+            % Generar H
+            H2(k,i)=1;
+            H2(k,j)=-M(i,j);
+            k=k+1;
+        end
+    end
 end
+
+H = [H2; ones(1, s(1))];
+
+% Resolver con minimos cuadrados
+w=H\b;
+
+% Normalizar
+w=w/sum(w);
+
+return
