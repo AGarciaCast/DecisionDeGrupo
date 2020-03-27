f9b82f2a13b0a9b8c90641a206fb21e51a6e08e0
Alejandro <a.gcastellanos@alumnos.upm.es>
2020-03-27 17:34:02 +0100

    Finalizar memoria 1

--- Código/casoPrueba.m
+++ Código/casoPrueba.m
@@ -10,6 +10,7 @@ for i = 1:4
 end
 
 M
+ic(M)
 fprintf("-------------Metodo de la potencia-------------\n");
 w0 = funciones(M, 0) % potencia(M)
 [errorInf, IndexMaxErr, errorFro, errorUno, errorErrRel] = errores(M,w0)
@@ -17,6 +18,7 @@ w0 = funciones(M, 0) % potencia(M)
 fprintf("-------------Metodo de Minimos Cuadrados [Log]-------------\n");
 w1 = funciones(M, 1) % minCuadLog(M)
 [errorInf, IndexMaxErr, errorFro, errorUno, errorErrRel] = errores(M,w1)
+
 fprintf("-------------Metodo de Minimos Cuadrados [Ponderado]-------------\n");
 w2 = funciones(M, 2) % minCuadPond(M)
 [errorInf, IndexMaxErr, errorFro, errorUno, errorErrRel] = errores(M,w2)
@@ -30,7 +32,7 @@ legend(l);
 clear;
 fprintf("-------------NO Consistente-------------\n");
 M=[1 2,2,4;1,1,1.5,3;0.5,0.6,1,2;0.25,0.3,0.5,1]
-
+ic(M)
 fprintf("-------------Metodo de la potencia-------------\n");
 w0 = funciones(M, 0) % potencia(M)
 [errorInf, IndexMaxErr, errorFro, errorUno, errorErrRel] = errores(M,w0)
